1e3acc5aa43be5c9f988e1dbb533fb05e56f7141
Wes Dean <Wesley.Dean@kdaweb.com>
2018-09-25 20:03:49 -0400

    continue line, dummy..

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+# This is our starting point, traditionally named 'main.tf'
+#
+# The majority of the configuration takes place in other
+# locations, including:
+#   * variables.tf : variables that can be set
+#   * data.tf      : data source queries
+#   * resources.tf : the resources (EC2 instance, etc.) created
+
 provider "aws" {
   region = "us-east-1"
 }
--- resources.tf
+++ resources.tf
@@ -1,3 +1,8 @@
+# in order to allow incoming web traffic, we need to open the appropriate
+# port; to do so, we'll use a Security Group that allows TCP traffic from
+# anywhere to the port while also allowing any outgoing traffic to anywhere
+# by any protocol
+
 resource "aws_security_group" "fcc" {
   name = "allow port incoming traffic"
 
@@ -17,6 +22,10 @@ resource "aws_security_group" "fcc" {
 }
 
 
+# now that we have a Security Group we can use, let's create an EC2 instance
+# that uses the AMI we queried before and use 'user_data' to start the
+# Flask development server at startup
+
 resource "aws_instance" "fcc" {
   ami = "${data.aws_ami.fcc_ami.image_id}"
   instance_type = "t2.micro"
@@ -26,6 +35,12 @@ resource "aws_instance" "fcc" {
     Name = "fcc"
   }
 }
+
+
+# finally, we also want to associate a DNS hostname (an alias created
+# through Route53) that points to the public IP address of the EC2 instance
+# we're running; the Time To Live (TTL) is set to 30 seconds so that if (when)
+# we rebuild the image, the old IP address won't live in the cache for a long time
 
 resource "aws_route53_record" "fcc" {
   zone_id = "${data.aws_route53_zone.primary.zone_id}"
